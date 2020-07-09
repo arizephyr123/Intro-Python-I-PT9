333b171b6c8654dabf07735767e4ae7cce8dad46
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-08 21:03:57 -0700

    hello, bignum, datatypes, modules completed

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,29 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print(sys.platform)
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print(sys.version_info)
+print(f'python{sys.version_info[0]}.{sys.version_info[1]}')
 
 
 import os
+import pwd
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(os.getlogin())
+uid = os.getuid()
+# print(uid)
+print(pwd.getpwuid(uid)[0])
