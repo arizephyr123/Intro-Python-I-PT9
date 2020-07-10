a4d267970717e0f05b4a46250ec369ca8a9f314a
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-09 19:00:21 -0700

    lists, tuples completed

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,28 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
-print(x)
+x.append(4)
+a=x
+print(a)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
-print(x)
+x.extend(y)
+b=x
+print(b)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE
-print(x)
+x.remove(8)
+c=x
+print(c)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE
-print(x)
+x.insert(-1, 99)
+d=x
+print(d)
 
 # Print the length of list x
-# YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+for num in x:
+    print(num * 1000)
