40e34b8ae06a391d6118af70c30d87977244f1a5
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-10 19:07:12 -0700

    dictionaries completed

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -13,7 +13,8 @@ y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
-print(x + int(y))
+print(type(x + int(y)), x + int(y))
+
 
 
 # Write a print statement that combines x + y into the string value 57
